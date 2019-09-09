ce1dd2f19271cf80909efa3a24f179c2b1c4c941
Denis Granha <denisgranha@gmail.com>
2019-09-09 17:34:48 +0200

    implement config for deployment params

---
+++ deploy-params.js
@@ -0,0 +1,6 @@
+module.exports = {
+  receiver: "0xe9c371eC2Ff267c7e63D3359576b367AADFC20Fa", // The person/project receiving the grant
+  wallet: "0x2851c5AbE017E969a22b38E9FD6736584D4Bb1e0", // Wallet allowed to withdraw funds if critaria for the grant is not achieved
+  disbursementPeriod: 3600*24*365, // Amount of time in seconds for the disbursement
+  startDate: 0 // Starting time for the distribution, (cliff)
+}
--- migrations/2_disbursement.js
+++ migrations/2_disbursement.js
@@ -1,7 +1,30 @@
 const Disbursement = artifacts.require('Disbursement.sol')
-const {receiver, wallet, disbursementPeriod, startDate} = require("../params.json")
+const {receiver, wallet, disbursementPeriod, startDate} = require("../deploy-params.js")
+const {isAddress} = require("web3-utils")
+const fourYears = 3600*24*365*4;
+const currentEpochInSeconds = Math.floor(new Date() / 1000)
 
 module.exports = deployer => {
   // Validate params
+  if (!receiver || !wallet || !disbursementPeriod || startDate == undefined){
+    throw Error("Fill the deployment parameters in deploy-params.js. All are required.")
+  }
+
+  if (!isAddress(receiver)){
+    throw Error(`receiver should be a valid ethereum address. Got ${receiver}`)
+  }
+
+  if (!isAddress(wallet)){
+    throw Error(`wallet should be a valid ethereum address. Got ${wallet}`)
+  }
+
+  if (disbursementPeriod > fourYears){
+    throw Error(`disbursement period should be in seconds and no longer than 4 years. Got ${disbursementPeriod}`)
+  }
+
+  if (startDate < currentEpochInSeconds && startDate != 0){
+    throw Error(`startDate should be either 0 or higher than current time. Got ${startDate}`)
+  }
+
   deployer.deploy(Disbursement, receiver, wallet, disbursementPeriod, startDate)
 }
