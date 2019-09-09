506430603795360500397719d9f136cf079c0862
Denis Granha <denisgranha@gmail.com>
2019-09-09 18:47:17 +0200

    mainnet deployment for first grant

--- deploy-params.js
+++ deploy-params.js
@@ -1,7 +1,11 @@
+const endDate = new Date("Jul 15 2020 00:00:00 UTC")
+
 module.exports = {
-  receiver: "0xe9c371eC2Ff267c7e63D3359576b367AADFC20Fa", // The person/project receiving the grant
-  wallet: "0x2851c5AbE017E969a22b38E9FD6736584D4Bb1e0", // Wallet allowed to withdraw funds if critaria for the grant is not achieved
-  disbursementPeriod: 3600*24*365, // Amount of time in seconds for the disbursement
+  receiver: "0x224CF0B963c59B95623b3Dd6ce07b4cE40F7b134", // The person/project receiving the grant
+  wallet: "0x52DF85E9De71aa1C210873bcF37EC46d36c99dc2", // Wallet allowed to withdraw funds if critaria for the grant is not achieved
+  // disbursementPeriod: 3600*24*365, // Amount of time in seconds for the disbursement
+  disbursementPeriod: Math.floor(endDate/1000 - new Date()/1000),
   startDate: 0, // Starting time for the distribution, (cliff)
   token: "0x6810e776880c02933d47db1b9fc05908e5386b96" // Token used for payout
+  // token: "0xc778417E063141139Fce010982780140Aa0cD5Ab"
 }
--- truffle-config.js
+++ truffle-config.js
@@ -69,6 +69,16 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.SEED, `https://node-green.mainnet.gnosis.pm`),
+      network_id: 1,       // Rinkeby's id
+      gas: 2e6,        // Rinkeby has a lower block limit than mainnet
+      gasPrice: 15e9,
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
